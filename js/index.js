fad5b3373948c07291cc2657cd5a6ee7c3528233
cslylla <81328857+cslylla@users.noreply.github.com>
2022-10-22 17:28:14 +0200

    Add current location and all locations to dropdown

--- js/index.js
+++ js/index.js
@@ -9,7 +9,9 @@ function updateTime() {
     let cityNameElement = localElement.querySelector(".cityName");
 
     cityNameElement.innerHTML = cityName;
-    localDateElement.innerHTML = moment().tz(timeZone).format("MMMM Mo, YYYY");
+    localDateElement.innerHTML = moment()
+      .tz(timeZone)
+      .format("dddd, MMMM Mo, YYYY");
     localTimeElement.innerHTML = moment()
       .tz(timeZone)
       .format("h:mm:ss [<small>]A[</small>]");
@@ -21,7 +23,7 @@ function updateTime() {
     let newYorkDateElement = newYorkElement.querySelector(".date");
     let newYorkTimeElement = newYorkElement.querySelector(".time");
     let newYorkTime = moment().tz("America/New_York");
-    newYorkDateElement.innerHTML = newYorkTime.format("MMMM Mo, YYYY");
+    newYorkDateElement.innerHTML = newYorkTime.format("dddd, MMMM Mo, YYYY");
     newYorkTimeElement.innerHTML = newYorkTime.format(
       "h:mm:ss [<small>]A[</small>]"
     );
@@ -37,13 +39,18 @@ let citySelectorElement = document.querySelector("#citySelector");
 function updateCity(event) {
   if (event.target.value.length > 0) {
     let timeZone = event.target.value;
+    if (event.target.value === "current") {
+      timeZone = moment.tz.guess();
+    }
     let city = timeZone.replace("_", " ").split("/")[1];
     let cityDateTime = moment.tz(timeZone);
     let newCityElement = document.querySelector("#new-city");
     newCityElement.innerHTML = `
             <div>
                 <h2>${city}</h2>
-                <div class="date">${cityDateTime.format("MMMM Mo, YYYY")}</div>
+                <div class="date">${cityDateTime.format(
+                  "dddd, MMMM Mo, YYYY"
+                )}</div>
             </div>
             <div class="time">${cityDateTime.format(
               "h:mm:ss"
