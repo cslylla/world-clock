6ba4385100db4d38f014257c518856566237c765
cslylla <81328857+cslylla@users.noreply.github.com>
2022-10-22 21:27:24 +0200

    Add dark theme and images

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Display city times with seconds
 function updateTime() {
   //Locale
   let localElement = document.querySelector("#local");
@@ -47,6 +48,18 @@ setInterval(updateTime, 1000);
 
 //City Selector
 let citySelectorElement = document.querySelector("#citySelector");
+function getImage() {
+  let images = [
+    "/src/clock_image_0.svg",
+    "/src/clock_image_1.svg",
+    "/src/clock_image_2.svg",
+    "/src/clock_image_3.svg",
+    "/src/clock_image_4.svg",
+    "/src/clock_image_5.svg",
+  ];
+  let randomNumber = Math.floor(Math.random() * 6);
+  return images[randomNumber];
+}
 
 function updateCity(event) {
   if (event.target.value.length > 0) {
@@ -67,16 +80,33 @@ function updateCity(event) {
                     )}</div>
                 </div>
                 <div class="time">${cityDateTime.format(
-                  "h:mm:ss"
+                  "h:mm"
                 )}<small>${cityDateTime.format("A")}</small></div>
           
       </div>
-      <img src="/src/clock_image.svg" alt="Clock drawing" class="clock-image">
-
-
-
+      <img src="${getImage()}" alt="Clock drawing" class="clock-image">
     `;
   }
 }
 
 citySelectorElement.addEventListener("change", updateCity);
+
+//Theme change
+let themeButtonElement = document.querySelector("#theme-button");
+let main = document.querySelector("main");
+
+function changeTheme() {
+  if (main.classList.contains("dark")) {
+    main.classList.remove("dark");
+    themeButtonElement.innerHTML = `<span class="material-symbols-outlined">
+dark_mode
+</span>`;
+  } else {
+    main.classList.add("dark");
+    themeButtonElement.innerHTML = `<span class="material-symbols-outlined">
+            light_mode
+        </span>`;
+  }
+}
+
+themeButtonElement.addEventListener("click", changeTheme);
